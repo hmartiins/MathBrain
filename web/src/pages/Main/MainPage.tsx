d75fae55221c2bd70e1690f9ae8ac1c856719551
Henrique Martins <hmartins224@gmail.com>
2020-06-05 22:45:04 -0300

    Adding buttons in the header and an illustration

--- web/src/pages/Main/MainPage.tsx
+++ web/src/pages/Main/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import Wave from './components/wave';
+import Ilustration from './images/mainImg.png'; 
 import styles from './styles.module.css';
 
 const Main: React.FC = () => {
@@ -8,11 +8,25 @@ const Main: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.contentContainer}>
         <div className={styles.content}>
-          <span className={styles.textLogo}>
-            MathBrain
-          </span>  
+          <div className={styles.headerContainer}>
+            <span className={styles.textLogo}> 
+              MathBrain
+            </span>
+            <div className={styles.headerRight}>
+              <span className={styles.spanHeader}>Sobre o porjeto</span>
+              <span className={styles.spanHeader}>Contato</span>
+              <span className={styles.spanHeader}>Sobre</span>
+              <span className={styles.myButton}>Baixe o app mobile</span>
+            </div>  
+          </div>
+          <div className={styles.illustration}>
+            <img 
+              src={Ilustration} 
+              className={styles.illustrationImg} 
+              alt="ilustration"
+            />
+          </div>
         </div>
-
       </div>      
     </div>
   );
